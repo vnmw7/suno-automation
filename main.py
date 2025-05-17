ae19991ea55f81944da2f8be2400636dd8c4ce88
vnmw7 (vincent) <146041946+vnmw7@users.noreply.github.com>
2025-05-17 13:06:58 +0800

    reverted back to AI prompt fot the chapter splitting

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-from pythonbible import Book
-
 from lib.supabase import supabase
-from utils.bible_utils import split_chapter_into_sections
-from utils.llm_chat_utils import create_song_structure
+from utils.llm_chat_utils import create_song_structure, get_verse_ranges
 
-book = Book.GENESIS
-book_name = str(book).replace("Book.", "")
+book_name = "Genesis"
 book_chapter = 1
 
-# prompt = f"Split {book_name} {book_chapter} in Christian NIV Bible into 2 sections of similar size which stand alone. If the chapter is between 30 and 45 verses long, split into 3 sections instead, and if the chapter is less than 15 verses, split into one section instead. If the chapter is between 45 and 60 verses long, split into 4 sections instead. If the chapter is over 60 verses long, split into 5 sections instead. give the range of versus separated by commas. Provide the output as numbers in oneline, nothing extra like explanations. Do not iclude the thinking and thought process to save output tokens."
+prompt = f"Split {book_name} {book_chapter} in Christian NIV Bible into 2 sections of similar size which stand alone. If the chapter is between 30 and 45 verses long, split into 3 sections instead, and if the chapter is less than 15 verses, split into one section instead. If the chapter is between 45 and 60 verses long, split into 4 sections instead. If the chapter is over 60 verses long, split into 5 sections instead. give the range of versus separated by commas. Provide the output as numbers in oneline, nothing extra like explanations. Do not iclude the thinking and thought process to save output tokens."
 
-verse_ranges = verse_ranges_enum = split_chapter_into_sections(book, str(book_chapter))
+verse_ranges = get_verse_ranges(prompt)
 verse_ranges = verse_ranges.split(",")
 print(type(verse_ranges))
 print(f"Verse ranges: {verse_ranges}")
